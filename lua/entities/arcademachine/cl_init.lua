4c0448ced26ec639809b10732fa8a02682a7ea52
ukgamer <ukgamer0@gmail.com>
2020-08-03 21:02:50 +0100

    Improvements to controls/info display
    Moving non-entity specific clientside stuff to own file

--- lua/entities/arcademachine/cl_init.lua
+++ lua/entities/arcademachine/cl_init.lua
@@ -1,13 +1,17 @@
+AM = AM or {
+    QueuedSounds = {}
+}
+
 include("shared.lua")
+include("cl_hooks.lua")
 
 local Debug = CreateClientConVar("arcademachine_debug", 0, true, false)
-local FOV = CreateClientConVar("arcademachine_fov", 70, true, false)
-local DisableBloom = CreateClientConVar("arcademachine_disable_bloom", 1, true, false)
-local DisablePAC = CreateClientConVar("arcademachine_disable_pac", 1, true, false)
---local DisableOutfitter = CreateClientConVar("arcademachine_disable_outfitter", 1, true, false)
-local DisableOthers = CreateClientConVar("arcademachine_disable_others_when_active", 0, true, false)
+AM.FOV = CreateClientConVar("arcademachine_fov", 70, true, false)
+AM.DisableBloom = CreateClientConVar("arcademachine_disable_bloom", 1, true, false)
+AM.DisablePAC = CreateClientConVar("arcademachine_disable_pac", 1, true, false)
+--AM.DisableOutfitter = CreateClientConVar("arcademachine_disable_outfitter", 1, true, false)
+AM.DisableOthers = CreateClientConVar("arcademachine_disable_others_when_active", 0, true, false)
 
-local LookDist = 100
 local MaxDist = 200
 
 local ScreenWidth = 512
@@ -25,8 +29,18 @@ local PACWasDisabled = false
 
 local LoadedLibs = {}
 
-local QueuedSounds = {}
-local NextQueueAt = 0
+surface.CreateFont("AMInfoFont", {
+    font = "Tahoma",
+    extended = true,
+    size = 16
+})
+
+surface.CreateFont("AMInfoFontBold", {
+    font = "Tahoma",
+    extended = true,
+    size = 16,
+    weight = 1000
+})
 
 local function DebugPrint(...)
     if Debug:GetBool() then
@@ -34,14 +48,7 @@ local function DebugPrint(...)
     end
 end
 
-local function ClearImageCache()
-    local path = "arcademachines/cache/images"
-    for _, v in ipairs(file.Find(path .. "/*", "DATA")) do
-        file.Delete(path .. "/" .. v)
-    end
-end
-
-local function ReloadMachines()
+concommand.Add("arcademachine_reload_machines", function()
     for _, v in ipairs(ents.FindByClass("arcademachine")) do
         if v:GetCurrentGame() then
             v:SetGame(v:GetCurrentGame())
@@ -54,197 +61,22 @@ local function ReloadMachines()
             end
         end
     end
-end
+end)
 
-AMSettingsPanel = AMSettingsPanel or nil
-local function ShowSettingsPanel()
-    if not IsValid(AMSettingsPanel) then
-        AMSettingsPanel = vgui.Create("DFrame")
-        AMSettingsPanel:SetSize(ScrW() * 0.15, ScrH() * 0.2)
-        AMSettingsPanel:SetMinimumSize(200, 200)
-        AMSettingsPanel:SetTitle("Arcade Machine Settings")
-        AMSettingsPanel:DockPadding(10, 30, 10, 10)
-    
-        local scroll = vgui.Create("DScrollPanel", AMSettingsPanel)
-        scroll:Dock(FILL)
+concommand.Add("arcademachine_clear_cache", function()
+    local paths = {
+        "images",
+        "files"
+    }
 
-        local label = vgui.Create("DLabel", scroll)
-        label:Dock(TOP)
-        label:SetWrap(true)
-        label:SetAutoStretchVertical(true)
-        label:DockMargin(0, 0, 0, 10)
-        label:SetFont("DermaDefaultBold")
-        label:SetText("General")
+    local base = "arcademachines/cache/"
 
-        local checkbox = vgui.Create("DCheckBoxLabel", scroll)
-        checkbox:Dock(TOP)
-        checkbox:DockMargin(0, 0, 0, 5)
-        checkbox:SetText("Disable bloom when in machine")
-        checkbox:SetConVar("arcademachine_disable_bloom")
-        checkbox:SetValue(DisableBloom:GetBool())
-        checkbox:SizeToContents()
-
-        local label = vgui.Create("DLabel", scroll)
-        label:Dock(TOP)
-        label:SetWrap(true)
-        label:SetAutoStretchVertical(true)
-        label:DockMargin(0, 0, 0, 10)
-        label:SetFont("DermaDefaultBold")
-        label:SetText("Performance")
-
-        if pac then
-            local checkbox = vgui.Create("DCheckBoxLabel", scroll)
-            checkbox:Dock(TOP)
-            checkbox:DockMargin(0, 0, 0, 5)
-            checkbox:SetText("Disable PAC when in machine")
-            checkbox:SetConVar("arcademachine_disable_pac")
-            checkbox:SetValue(DisablePAC:GetBool())
-            checkbox:SizeToContents()
-        end
-
-        local checkbox = vgui.Create("DCheckBoxLabel", scroll)
-        checkbox:Dock(TOP)
-        checkbox:DockMargin(0, 0, 0, 5)
-        checkbox:SetText("Disable other machines when in machine")
-        checkbox:SetConVar("arcademachine_disable_others_when_active")
-        checkbox:SetValue(DisableOthers:GetBool())
-        checkbox:SizeToContents()
-
-        local label = vgui.Create("DLabel", scroll)
-        label:Dock(TOP)
-        label:SetWrap(true)
-        label:SetAutoStretchVertical(true)
-        label:DockMargin(0, 0, 0, 10)
-        label:SetFont("DermaDefaultBold")
-        label:SetText("Debug")
-
-        local button = vgui.Create("DButton", scroll)
-        button:Dock(TOP)
-        button:DockMargin(0, 0, 0, 5)
-        button:SetText("Clear image cache")
-        button.DoClick = function()
-            ClearImageCache()
-        end
-
-        local button = vgui.Create("DButton", scroll)
-        button:Dock(TOP)
-        button:DockMargin(0, 0, 0, 5)
-        button:SetText("Reload machines")
-        button.DoClick = function()
-            ReloadMachines()
+    for _, v in ipairs(paths) do
+        for _, fv in ipairs(file.Find(base .. v .. "/*", "DATA")) do
+            file.Delete(base .. v .. "/" .. fv)
         end
     end
-
-    AMSettingsPanel:Center()
-    AMSettingsPanel:MakePopup()
-end
-
-list.Set("DesktopWindows", "ArcadeMachines", {
-    title = "Arcade Machines",
-    icon = "icon64/arcademachine.png",
-    init = function(icon, window)
-        ShowSettingsPanel()
-    end
-})
-
-AMInfoPanel = AMInfoPanel or nil
-local LookingAt = nil
-local function ShowInfoPanel(machine)
-    if LookingAt == machine then return end
-
-    if LookingAt ~= machine and IsValid(AMInfoPanel) then
-        AMInfoPanel:Remove()
-    end
-
-    LookingAt = machine
-
-    local bg = Color(0, 0, 0, 200)
-
-    AMInfoPanel = vgui.Create("DFrame")
-    AMInfoPanel:SetPaintedManually(true)
-    AMInfoPanel:SetSize(ScrW() * 0.15, ScrH() * 0.2)
-    AMInfoPanel:SetMinimumSize(300, 300)
-    AMInfoPanel:SetPos(0, ScrH() * 0.5 - (AMInfoPanel:GetTall() * 0.5))
-    AMInfoPanel:SetTitle("")
-    AMInfoPanel:SetDraggable(false)
-    AMInfoPanel:ShowCloseButton(false)
-    AMInfoPanel:DockPadding(10, 10, 10, 20)
-    AMInfoPanel.Paint = function(self, w, h)
-        draw.RoundedBoxEx(20, 0, 0, w, h, bg, false, true, false, true)
-
-        local text = "Open chat and mouse over to scroll (default Y)"
-
-        surface.SetTextColor(200, 200, 200, 255)
-        surface.SetFont("DermaDefaultBold")
-        local tw, th = surface.GetTextSize(text)
-        surface.SetTextPos(w * 0.5 - (tw * 0.5), h - th - 5)
-        surface.DrawText(text)
-    end
-
-    local scroll = vgui.Create("DScrollPanel", AMInfoPanel)
-    scroll:Dock(FILL)
-    local sbar = scroll:GetVBar()
-    sbar.Paint = function(self, w, h) end
-    sbar.btnUp.Paint = function(self, w, h) end
-    sbar.btnDown.Paint = function(self, w, h) end
-    sbar.btnGrip.Paint = function(self, w, h) end
-
-    if machine.Game then
-        local label = vgui.Create("DLabel", scroll)
-        label:Dock(TOP)
-        label:SetWrap(true)
-        label:SetAutoStretchVertical(true)
-        label:DockMargin(0, 0, 0, 15)
-        label:SetFont("DermaLarge")
-        label:SetText(machine.Game.Name)
-    end
-
-    local cost = machine:GetMSCoinCost()
-
-    if cost > 0 then
-        local label = vgui.Create("DLabel", scroll)
-        label:Dock(TOP)
-        label:SetWrap(true)
-        label:SetAutoStretchVertical(true)
-        label:DockMargin(0, 0, 0, 15)
-        label:SetFont("DermaDefault")
-        label:SetText("This machine costs " .. cost .. " coin(s) to play.")
-    end
-
-    local label = vgui.Create("DLabel", scroll)
-    label:Dock(TOP)
-    label:SetWrap(true)
-    label:SetAutoStretchVertical(true)
-    label:DockMargin(0, 0, 0, 10)
-    label:SetFont("DermaDefaultBold")
-    label:SetText("Instructions/Controls")
-
-    local label = vgui.Create("DLabel", scroll)
-    label:Dock(TOP)
-    label:SetWrap(true)
-    label:SetAutoStretchVertical(true)
-    label:DockMargin(0, 0, 0, 15)
-    label:SetFont("DermaDefault")
-    label:SetText("Press your WALK key (default ALT) to insert coins. Use scroll wheel to zoom. Hold USE (default E) to exit (you will lose any ununsed coins!). Settings can be found in the context menu (default C).")
-
-    if machine.Game and machine.Game.Description then
-        local label = vgui.Create("DLabel", scroll)
-        label:Dock(TOP)
-        label:SetWrap(true)
-        label:SetAutoStretchVertical(true)
-        label:DockMargin(0, 0, 0, 10)
-        label:SetFont("DermaDefaultBold")
-        label:SetText("Game Information")
-
-        local label = vgui.Create("DLabel", scroll)
-        label:Dock(TOP)
-        label:SetWrap(true)
-        label:SetAutoStretchVertical(true)
-        label:DockMargin(0, 0, 0, 15)
-        label:SetFont("DermaDefault")
-        label:SetText(machine.Game.Description)
-    end
-end
+end)
 
 local BG = {
     BG_GENERIC_JOYSTICK = 0,
@@ -261,8 +93,6 @@ local Bodygroups = {
     [BG.BG_GENERIC_RECESSED_TRACKBALL] = { 1, 2 },
     [BG.BG_DRIVING] = { 2, 3 }
 }
-
-AMCurrentMachine = AMCurrentMachine or nil
 
 local function WrappedInclusion(path, upvalues)
     local gameMeta = setmetatable(upvalues, { __index = _G, __newindex = _G })
@@ -393,7 +223,7 @@ function ENT:Think()
         self.Entity:SetBodygroup(1, Bodygroups[self.Game.Bodygroup][2])
     end
 
-    if DisableOthers:GetBool() and AMCurrentMachine and AMCurrentMachine ~= self then
+    if AM.DisableOthers:GetBool() and AM.CurrentMachine and AM.CurrentMachine ~= self then
         return
     end
 
@@ -453,7 +283,7 @@ end
 function ENT:Draw()
     local marqueeIndex = self.Entity:GetBodygroup(0)
 
-    if IsValid(AMCurrentMachine) and AMCurrentMachine == self and not LocalPlayer():ShouldDrawLocalPlayer() then
+    if IsValid(AM.CurrentMachine) and AM.CurrentMachine == self and not LocalPlayer():ShouldDrawLocalPlayer() then
         cam.IgnoreZ(true)
     end
 
@@ -464,11 +294,11 @@ function ENT:Draw()
     self.Entity:DrawModel()
     render.MaterialOverrideByIndex()
 
-    if IsValid(AMCurrentMachine) and AMCurrentMachine == self and not LocalPlayer():ShouldDrawLocalPlayer() then
+    if IsValid(AM.CurrentMachine) and AM.CurrentMachine == self and not LocalPlayer():ShouldDrawLocalPlayer() then
         cam.IgnoreZ(false)
     end
 
-    if not self.InRange or not self.Game or (DisableOthers:GetBool() and AMCurrentMachine and AMCurrentMachine ~= self) then
+    if not self.InRange or not self.Game or (AM.DisableOthers:GetBool() and AM.CurrentMachine and AM.CurrentMachine ~= self) then
         return
     end
 
@@ -507,21 +337,21 @@ function ENT:OnPlayerChange(name, old, new)
 end
 
 function ENT:OnLocalPlayerEntered()
-    AMCurrentMachine = self
+    AM.CurrentMachine = self
 
-    if DisableBloom:GetBool() and not cvars.Bool("mat_disable_bloom") then
+    if AM.DisableBloom:GetBool() and not cvars.Bool("mat_disable_bloom") then
         LocalPlayer():ConCommand("mat_disable_bloom 1")
         BloomWasDisabled = true
     end
 
-    if DisablePAC:GetBool() and pac and pac.IsEnabled() then
+    if AM.DisablePAC:GetBool() and pac and pac.IsEnabled() then
         pac.Disable()
         PACWasDisabled = true
     else
         PACWasDisabled = false
     end
 
-    --[[if DisableOutfitter:GetBool() and outfitter then
+    --[[if AM.DisableOutfitter:GetBool() and outfitter then
         outfitter.SetHighPerf(true, true)
         outfitter.DisableEverything()
         OutfitterWasDisabled = true
@@ -531,17 +361,17 @@ function ENT:OnLocalPlayerEntered()
 end
 
 function ENT:OnLocalPlayerLeft()
-    AMCurrentMachine = nil
+    AM.CurrentMachine = nil
 
-    if DisableBloom:GetBool() and BloomWasDisabled then
+    if AM.DisableBloom:GetBool() and BloomWasDisabled then
         LocalPlayer():ConCommand("mat_disable_bloom 0")
     end
 
-    if DisablePAC:GetBool() and PACWasDisabled then
+    if AM.DisablePAC:GetBool() and PACWasDisabled then
         pac.Enable()
     end
 
-    --[[if DisableOutfitter:GetBool() and OutfitterWasDisabled then
+    --[[if AM.DisableOutfitter:GetBool() and OutfitterWasDisabled then
         outfitter.SetHighPerf(false, true)
         outfitter.EnableEverything()
     end--]]
@@ -621,8 +451,8 @@ function ENT:StopSounds()
     end
 
     table.Empty(self.LoadedSounds)
-    if QueuedSounds[self:EntIndex()] then
-        QueuedSounds[self:EntIndex()] = nil
+    if AM.QueuedSounds[self:EntIndex()] then
+        AM.QueuedSounds[self:EntIndex()] = nil
     end
 end
 
@@ -659,7 +489,7 @@ function ENT:GetUpvalues(game)
 
     -- Allow each instance to have its own copy of sound library in case they want to
     -- play the same sound at the same time (needs to emit from the machine)
-    upvalues.SOUND = WrappedInclusion("arcademachine_lib/sound.lua", { MACHINE = self, QUEUE = QueuedSounds })
+    upvalues.SOUND = WrappedInclusion("arcademachine_lib/sound.lua", { MACHINE = self, QUEUE = AM.QueuedSounds })
 
     upvalues.COINS = WrappedInclusion("arcademachine_lib/coins.lua", { MACHINE = self })
     upvalues.MARQUEE = WrappedInclusion("arcademachine_lib/marquee.lua", { MACHINE = self })
@@ -684,7 +514,7 @@ function ENT:SetGame(game, forceLibLoad)
             self.Game:Init()
         end
 
-        if IsValid(self:GetPlayer()) and self:GetPlayer() == LocalPlayer() then
+        if IsValid(self:GetPlayer()) then
             self.Game:OnStartPlaying(self:GetPlayer())
         end
     end
@@ -695,136 +525,3 @@ function ENT:SetGame(game, forceLibLoad)
     end
     self:UpdateScreen()
 end
-
-hook.Add("CalcVehicleView", "arcademachine_view", function(veh, ply, view)
-    if not IsValid(AMCurrentMachine) then
-        return
-    end
-
-    local tp = veh.GetThirdPersonMode and veh:GetThirdPersonMode() or false
-
-    if tp then return end
-
-    if AMCurrentMachine:GetBodygroup(0) == 1 then
-        view.origin = veh:GetPos() + veh:GetRight() * -8 + veh:GetUp() * 72
-    else
-        view.origin = veh:GetPos() + veh:GetUp() * 64
-    end
-
-    view.fov = FOV:GetInt()
-
-    return view
-end)
-
-hook.Add("CreateMove", "arcademachine_scroll", function(cmd)
-    if not IsValid(AMCurrentMachine) then
-        PressedUse = false
-        return
-    end
-
-    local fov = FOV:GetInt()
-
-    if cmd:GetMouseWheel() < 0 and fov < 100 then
-        FOV:SetInt(fov + 2)
-    end
-    if cmd:GetMouseWheel() > 0 and fov > 40 then
-        FOV:SetInt(fov - 2)
-    end
-
-    if bit.band(cmd:GetButtons(), IN_USE) ~= 0 then
-        if not PressedUse then
-            PressedUse = true
-            PressedUseAt = RealTime()
-        elseif RealTime() >= PressedUseAt + 0.8 then
-            net.Start("arcademachine_leave")
-            net.SendToServer()
-            PressedUse = false
-        end
-    else
-        PressedUse = false
-    end
-end)
-
-hook.Add("ScoreboardShow", "arcademachine_scoreboard", function()
-    if not IsValid(AMCurrentMachine) then return end
-
-    return false
-end)
-
-local notificationColor = Color(255, 255, 255)
-hook.Add("HUDPaint", "arcademachine_hud", function()
-    -- Paint manually so the panel hides when the camera is out
-    if IsValid(AMInfoPanel) then
-        AMInfoPanel:PaintManual()
-    end
-
-    if PressedUse then
-        notificationColor.a = 50 + math.abs(math.sin(RealTime() * 10) * 205)
-
-        draw.DrawText("Keep holding USE to exit the machine!", "DermaLarge", ScrW() * 0.5, ScrH() * 0.3, notificationColor, TEXT_ALIGN_CENTER)
-    end
-end)
-
-hook.Add("Think", "arcademachine_think", function()
-    -- In case the player gets pulled out of the machine somehow
-    if IsValid(AMCurrentMachine) and AMCurrentMachine:GetPlayer() ~= LocalPlayer() then
-        AMCurrentMachine = nil
-    end
-
-    if not IsValid(AMCurrentMachine) then
-        local tr = util.TraceLine(util.GetPlayerTrace(LocalPlayer(), EyeAngles():Forward()))
-
-        if
-            IsValid(tr.Entity) and
-            tr.Entity:GetClass() == "arcademachine" and
-            tr.Entity:GetPos():DistToSqr(LocalPlayer():GetPos()) < LookDist * LookDist
-        then
-            ShowInfoPanel(tr.Entity)
-        else
-            if IsValid(AMInfoPanel) then
-                AMInfoPanel:Remove()
-            end
-            LookingAt = nil
-        end
-    else
-        if IsValid(AMInfoPanel) then
-            AMInfoPanel:Remove()
-        end
-        LookingAt = nil
-    end
-
-    if RealTime() < NextQueueAt then return end
-
-    local k, v = next(QueuedSounds)
-
-    if k then
-        if #v > 0 then
-            v[1].context:LoadQueued(v[1])
-            table.remove(v, 1)
-        else
-            QueuedSounds[k] = nil
-        end
-    end
-
-    NextQueueAt = RealTime() + 0.05
-end)
-
-hook.Add("PrePlayerDraw", "arcademachine_hideplayers", function(ply)
-    if not IsValid(AMCurrentMachine) or ply == LocalPlayer() then return end
-
-    local min, max = LocalPlayer():WorldSpaceAABB()
-
-    return ply:GetPos():WithinAABox(min, max)
-end)
-
-hook.Add("HUDDrawTargetID", "arcademachine_hideplayers", function()
-    if not IsValid(AMCurrentMachine) then return end
-
-    return false
-end)
-
-hook.Add("ContextMenuOpen", "arcademachine_nocontextmenu", function()
-    if not IsValid(AMCurrentMachine) then return end
-
-    return false
-end)
