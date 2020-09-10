b035b15b45a4aff7ac9ada75d253d609ac525ab2
ukgamer <ukgamer0@gmail.com>
2020-09-10 18:55:20 +0100

    Turn prediction back on for strikers

--- lua/entities/arcade_airhockey_striker/cl_init.lua
+++ lua/entities/arcade_airhockey_striker/cl_init.lua
@@ -1,5 +1,19 @@
 include("shared.lua")
 
+ENT.Initialized = false
+
+function ENT:Initialize()
+    self.Initialized = true
+
+    self:SetPredictable(true)
+end
+
+function ENT:Think()
+    if not self.Initialized then
+        self:Initialize()
+    end
+end
+
 function ENT:Draw()
     local ignoreZ = IsValid(ARCADE.AirHockey.CurrentMachine) and
         (ARCADE.AirHockey.CurrentMachine:GetStriker1() == self or ARCADE.AirHockey.CurrentMachine:GetStriker2() == self) and
--- lua/entities/arcade_airhockey_striker/shared.lua
+++ lua/entities/arcade_airhockey_striker/shared.lua
@@ -9,6 +9,7 @@ ENT.Class = "arcade_airhockey_striker"
 
 function ENT:SetupDataTables()
     self:NetworkVar("Entity", 0, "AirHockeyTable")
+    self:NetworkVar("Vector", 0, "MoveVector")
 end
 
 function ENT:CanConstruct()
@@ -51,13 +52,15 @@ local LocalBoundary2 = {
 function ENT:PhysicsSimulate(phys, delta)
     phys:Wake()
 
+    if not self.GetMoveVector then return end
+
     local hockeyTable = self:GetAirHockeyTable()
 
     if not IsValid(hockeyTable) then return end
 
     local origin = hockeyTable:LocalToWorld(hockeyTable:GetStriker1() == self and StartPos1 or StartPos2)
 
-    phys:UpdateShadow(self.MoveVector and origin + self.MoveVector or origin, Angle(0, hockeyTable:GetAngles().y), FrameTime())
+    phys:UpdateShadow(self:GetMoveVector() and origin + self:GetMoveVector() or origin, Angle(0, hockeyTable:GetAngles().y), FrameTime())
 end
 
 hook.Add("StartCommand", "arcade_airhockey_move", function(ply, cmd)
@@ -67,13 +70,15 @@ hook.Add("StartCommand", "arcade_airhockey_move", function(ply, cmd)
 
     local striker = veh.AirHockey:GetPlayer1() == ply and veh.AirHockey:GetStriker1() or veh.AirHockey:GetStriker2()
 
+    if not striker.GetMoveVector then return end
+
     -- TODO: Add configurable mouse sensitivity
     local mouseX = math.Clamp(cmd:GetMouseX() * 0.1, -3, 3)
     local mouseY = math.Clamp(cmd:GetMouseY() * 0.1, -3, 3)
 
     local mult = veh.AirHockey:GetPlayer1() == ply and -1 or 1
 
-    local targetPos = (striker.MoveVector or Vector()) + (veh.AirHockey:GetForward() * mouseX * mult) + (veh.AirHockey:GetRight() * mouseY * mult)
+    local targetPos = (striker:GetMoveVector() or Vector()) + (veh.AirHockey:GetForward() * mouseX * mult) + (veh.AirHockey:GetRight() * mouseY * mult)
 
     local absTargetPos = veh.AirHockey:WorldToLocal(veh.AirHockey:LocalToWorld(striker == veh.AirHockey:GetStriker1() and StartPos1 or StartPos2) + targetPos)
 
@@ -81,6 +86,6 @@ hook.Add("StartCommand", "arcade_airhockey_move", function(ply, cmd)
         (striker == veh.AirHockey:GetStriker1() and ARCADE:WithinBounds(absTargetPos, LocalBoundary1)) or
         (striker == veh.AirHockey:GetStriker2() and ARCADE:WithinBounds(absTargetPos, LocalBoundary2))
     then
-        striker.MoveVector = targetPos
+        striker:SetMoveVector(targetPos)
     end
 end)
